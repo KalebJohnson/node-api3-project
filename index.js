f425dca2db575e8534144a75bc6cb2e8ea122755
unknown <KalebJohnson45@yahoo.com>
2020-08-17 23:14:54 -0500

    ugh

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
 const express = require("express")
 
 const userRouter = require("./users/userRouter")
 const postsRouter = require("./posts/postRouter")
+const welcomeRouter = require("./welcome/welcomeRouter")
 //const morgan = require("morgan")
 const logger = require("./users/logger")
 
 
 const server = express()
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 6000;
 
 server.use(express.json())
 
@@ -18,6 +18,7 @@ server.use(logger())
 //connecting routers
 server.use("/users" , userRouter)
 server.use("/posts" , postsRouter)
+server.use("/" , welcomeRouter)
 
 server.listen(port, () => {
 	console.log(`Server running on ${port}`)
