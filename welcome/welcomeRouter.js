a70305070a358c3d86df64c704105044ed44202e
unknown <KalebJohnson45@yahoo.com>
2020-08-19 19:32:12 -0500

    dif env var

--- welcome/welcomeRouter.js
+++ welcome/welcomeRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     res.status(200).json({
-        message: ` Jesus heroku is ${process.env.HEROKU || "ANNOYING"}`
+        message: ` my username is${process.env.USERNAME}`
         // $(process.env.HEROKU) variable wont be in my env unless I restart PC (cool windows)
     })
 })
